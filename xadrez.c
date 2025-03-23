cbf6ed01e620187d9cf8c583efb56604c1b7c2d3
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2025-03-23 17:53:13 +0000

    adicionei mov cavalo

--- xadrez.c
+++ xadrez.c
@@ -5,7 +5,7 @@ int main() {
     const int casasTorre = 5;
     const int casasBispo = 5;
     const int casasRainha = 8;
-
+    
     // Movimento da Torre: 5 casas para a direita
     printf("Movimento da Torre:\n");
     for (int t = 0; t < casasTorre; t++) {
@@ -27,6 +27,21 @@ int main() {
         printf("Esquerda\n");
         r++;
     } while (r < casasRainha);
+
+    // Separação dos movimentos do Cavalo
+    printf("\nMovimento do Cavalo:\n");
+    
+    // Definindo o número de casas a serem movidas pelo Cavalo
+    int movimentoCompleto = 1; // Flag para controlar o movimento em 'L'
+
+    // Loop para o movimento do Cavalo
+    while (movimentoCompleto--) {
+        // Loop para mover duas casas para cima
+        for (int i = 0; i < 2; i++) {
+            printf("Cima\n"); // Imprime "Cima" duas vezes
+        }
+        printf("Direita\n"); // Imprime "Direita" uma vez
+    }
 
     return 0;
 }
